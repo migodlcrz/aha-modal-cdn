6a60a76611c4535751ce3b1992a5e1a641dd4776
mikeeDaw <michaeldao05@gmail.com>
2024-05-31 04:19:06 +0800

    folder drawer initial

--- js/supportModal.js
+++ js/supportModal.js
@@ -6,60 +6,87 @@
 
   const modalId = `support-modal-${seedId()}`;
   const supportFormId = `support-form-${seedId()}`;
+  const overlayHtml = `
+    <div id="overlay" />
+  `;
   const modalHtml = `
     <div id="${modalId}" class="modal">
-        <div class="modal-content">
-            <span class="close">&times;</span>
-            <h2>Support</h2>
-            <form id="${supportFormId}">
-                <label for="issueType">Issue Type:</label>
-                <select id="issueType">
-                    <option value="modify">Modify</option>
-                    <option value="bug">Bug</option>
-                    <option value="feature-request">Feature Request</option>
-                    <option value="other">Other</option>
-                </select>
-                <label for="description">Description:</label>
-                <textarea id="description" rows="4" cols="50"></textarea>
-                <button type="submit">Submit</button>
-            </form>
-        </div>
+        X
     </div>
     `;
+  const wholeModal = `
+    <div id='supportModal' style='right: -14rem'>
+      <div id='contentWrapper'>
+      <button id="modalDraw">
+        <span> Support Modal </span>
+        <span> </span>
+      </button>
+      <div id="modalContent">
+        jkasdjakdsajkjkaskdjks
+      <div>
+      <div>
 
+    </div>
+  `;
+  const floatingButton = document.createElement("button");
+
+  document.body.insertAdjacentHTML("beforeend", wholeModal);
   document.body.insertAdjacentHTML("beforeend", modalHtml);
 
   const modal = document.getElementById(modalId);
-  const closeButton = modal.querySelector(".close");
-  const submitButton = modal.querySelector(`#${supportFormId} button`);
+  // const overlay = document.getElementById("overlay");
+  // overlay.addEventListener("click", closeOverlay);
+  // const closeButton = modal.querySelector(".close");
+  // const submitButton = modal.querySelector(`#${supportFormId} button`);
+  const drawer = document.getElementById("supportModal");
+  const drawerBTN = document.getElementById("modalDraw");
 
-  function openModal() {
-    modal.style.display = "block";
+  drawerBTN.addEventListener("click", () => {
+    let rVal = drawer.style.right;
+    if (rVal === "-14rem") {
+      drawer.style.right = "-1rem";
+    } else {
+      drawer.style.right = "-14rem";
+    }
+  });
+
+  function openModal(buttonElem) {
+    // modal.style.display = "block";
+    // overlay.style.display = "block";
+    buttonElem.style.width = "500px";
+    buttonElem.style.height = "200px";
   }
 
   function closeModal() {
     modal.style.display = "none";
   }
 
+  function closeOverlay() {
+    // overlay.style.display = "none";
+    floatingButton.style.width = "200px";
+    floatingButton.style.height = "50px";
+  }
+
   document.addEventListener("DOMContentLoaded", function () {
-    const floatingButton = document.createElement("button");
     floatingButton.innerText = "Support";
     floatingButton.id = "support-button";
     floatingButton.classList.add("floating-button");
     document.body.appendChild(floatingButton);
-    floatingButton.addEventListener("click", openModal);
+    floatingButton.addEventListener("click", () => {
+      openModal(floatingButton);
+    });
   });
 
   closeButton.addEventListener("click", closeModal);
 
   // Submit form
-  document
-    .getElementById(supportFormId)
-    .addEventListener("submit", function (e) {
-      e.preventDefault();
+  // document
+  //   .getElementById(supportFormId)
+  //   .addEventListener("submit", function (e) {
+  //     e.preventDefault();
 
-      // Here you can add logic to handle form submission (e.g., send data to server)
-      alert("Form submitted!");
-      closeModal();
-    });
+  //     // Here you can add logic to handle form submission (e.g., send data to server)
+  //     alert("Form submitted!");
+  //     closeModal();
+  //   });
 })();
